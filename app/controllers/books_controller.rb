4d1180800ea81db6e95659b44c0b9aa9057701fa
Haruto0309 <sato.haruto.infratop@gmail.com>
2024-09-19 11:00:36 +0000

    create機能の作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,34 @@
 class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "投稿に成功しました。"
+      redirect_to book_path(@book.id)
+    else
+      flash.now[:alert] = "投稿に失敗しました。"
+      render :index
+    end
+  end
+
   def index
+    @book = Book.new
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   get "/" => "homes#top"
-  get "books" => "books#index"
-  get "books/:id" => "books#show"
-  get "books/:id/edit" => "books#edit"
+  resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
