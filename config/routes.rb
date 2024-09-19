bb7f7b975431c885da7b1d8223dd72de6936b1a8
Haruto0309 <sato.haruto.infratop@gmail.com>
2024-09-19 05:41:33 +0000

    topページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
-  get 'homes/top'
+  get "/" => "homes#top"
+  get "books" => "books#index"
+  get "books/:id" => "books#show"
+  get "books/:id/edit" => "books#edit"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
